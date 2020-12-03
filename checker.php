358b4ab8c849a0457203e768f0008a3e1964344a
Alfredo <alfaradayepez@gmail.com>
2020-12-02 21:06:54 -0500

    Consumiendo datos de la API de paypal con curl de php

--- checker.php
+++ checker.php
@@ -1,3 +1,23 @@
 <?php
 
 print_r($_GET);
+
+$clientId = "AXlAuQZdeYFxfSn4n5bDvvu6EGbeYAMQnT6XSd7v4C8CNzDa6PWIJOx56MiwatzyBjK5RVHekA-2WfaF";
+$secret = "EBJyjKAO3VUKUo1zVZ23d-iATTOErOUA2XLjtEf0dIKKFa0iLiPHPAafUasJBnBexCvwM4r65Yh6gQet";
+
+$login = curl_init("https://api-m.sandbox.paypal.com/v1/oauth2/token");
+
+curl_setopt($login, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($login, CURLOPT_USERPWD, $clientId . ":". $secret);
+
+curl_setopt($login, CURLOPT_POSTFIELDS,"grant_type=client_credentials");
+
+$reply = curl_exec($login);
+
+$responseObject = json_decode($reply);
+
+$accessToken = $responseObject->access_token;
+
+print_r($accessToken);
+
