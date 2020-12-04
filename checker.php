f2f59ddf24a9502e4005a1abc8cd50ab0162384c
Alfredo <alfaradayepez@gmail.com>
2020-12-03 19:41:40 -0500

    Recuperando datos

--- checker.php
+++ checker.php
@@ -1,6 +1,6 @@
 <?php
 
-print_r($_GET);
+// print_r($_GET);
 
 $clientId = "AXlAuQZdeYFxfSn4n5bDvvu6EGbeYAMQnT6XSd7v4C8CNzDa6PWIJOx56MiwatzyBjK5RVHekA-2WfaF";
 $secret = "EBJyjKAO3VUKUo1zVZ23d-iATTOErOUA2XLjtEf0dIKKFa0iLiPHPAafUasJBnBexCvwM4r65Yh6gQet";
@@ -19,15 +19,27 @@ $responseObject = json_decode($reply);
 
 $accessToken = $responseObject->access_token;
 
-print_r($accessToken);
-
+// print_r($accessToken);
 
 $sale = curl_init("https://api.sandbox.paypal.com/v1/payments/payment/".$_GET['paymentID']);
 
 curl_setopt($sale,CURLOPT_HTTPHEADER, array("Content-Type: application/json","Authorization: Bearer ".$accessToken));
 
+curl_setopt($sale, CURLOPT_RETURNTRANSFER, true);
+
 $saleResponse = curl_exec($sale);
 
-$jsonResponse = json_decode($saleResponse);
+// print_r($saleResponse);
 
-print_r($jsonResponse);
+$objectTransactionData = json_decode($saleResponse); 
+
+// print_r($objectTransactionData);
+
+$state = $objectTransactionData->state;
+$email = $objectTransactionData->payer->payer_info->email;
+
+$total = $objectTransactionData->transactions[0]->amount->total;
+$currency = $objectTransactionData->transactions[0]->amount->currency;
+$custom = $objectTransactionData->transactions[0]->custom;
+
+echo "$total <br> $currency <br> $custom";
